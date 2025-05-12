5ddfacb719e6f998271ee36f32efe14b19140d92
nyxamo <nyxamo@gmail.com>
2025-05-12 16:50:56 -0400

    Partner Introduction - Git Intro

---
+++ gitIntro/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2> Hello my name is Nixie. I will be introducing my partner Marin</h2>";
+        
+        echo "</br>1 - Name and major / reason for taking the class</br>
+            My name is Marin Xhemollari. I’m majoring in Computer Information Systems at OCC,
+            and I’m taking this class to strengthen my web development skills and prepare for 
+            a smooth transfer to university. I’m planning to transfer with my associate degree 
+            and classes recommended from the universities in Fall 2026, either to UM-Dearborn or
+            Wayne State for Computer Science or Cybersecurity. I enjoy both fields because I like
+            using logic in creative ways to solve problems, build things, and combine ideas.</br>";
+
+        echo  "</br>2 - Any previous programming experience?</br>
+                I’ve worked with Java (my favorite so far), HTML, CSS, JavaScript, C++, 
+                and a bit of SQL.</br>";
+
+        echo "</br> 3 - Hobbies?</br>
+                I collect precious metals.</br>";
+
+        echo "</br> 4 - Winter plans aside from school?</br>
+                Not really—winter gets pretty busy during the 
+                short two-week break. I usually prefer traveling 
+                in the summer.</br>";
+
+        echo "</br> 5 - Anything else to share with the class?</br>
+                Nothing comes to mind right now, to be honest.</br>";
+        ?>
+    </body>
+</html>
